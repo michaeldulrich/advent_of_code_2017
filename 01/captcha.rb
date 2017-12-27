c9ad58484715ffc64f012d73f242e3b5fdb1d9a7
Michael Ulrich <michaeldulrich@gmail.com>
2017-12-27 09:30:49 -0500

    Removed Captcha class

--- 01/captcha.rb
+++ 01/captcha.rb
@@ -1,18 +1,15 @@
-class Captcha < File
-  input = Captcha.read('puzzle_input.txt').strip.chars.map(&:to_i)
-  sum = 0
+input = File.read('puzzle_input.txt').strip.chars.map(&:to_i)
+sum = 0
 
-  input.each_with_index do |x, i|
-    next_item = input[i+1]
-    if x == next_item
-      sum += x
-    end
+input.each_with_index do |x, i|
+  next_item = input[i+1]
+  if x == next_item
+    sum += x
   end
-
-  if input.first == input.last
-    sum += input.first
-  end
-
-  puts sum
-
 end
+
+if input.first == input.last
+  sum += input.first
+end
+
+puts sum
