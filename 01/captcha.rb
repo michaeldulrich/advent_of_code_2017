05176ce85a439a6daa1578b71da166dbc4c76805
Michael Ulrich <michaeldulrich@gmail.com>
2017-12-27 09:02:53 -0500

    Day 1 Part 1 solution
    It works!

--- 01/captcha.rb
+++ 01/captcha.rb
@@ -2,7 +2,17 @@ class Captcha < File
   input = Captcha.read('puzzle_input.txt').strip.chars.map(&:to_i)
   sum = 0
 
-  puts input.length
-
-
+  input.each_with_index do |x, i|
+    next_item = input[i+1]
+    if x == next_item
+      sum += x
+    end
+  end
+  
+  if input.first == input.last
+    sum += input.first
+  end
+  
+  puts sum
+  
 end
