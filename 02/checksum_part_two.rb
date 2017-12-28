32e7ee3ed96348ada228418a7606176176d6718a
Michael Ulrich <michaeldulrich@gmail.com>
2017-12-28 17:09:47 -0500

    Day 2, part 2 complete!

---
+++ 02/checksum_part_two.rb
@@ -0,0 +1,15 @@
+input = File.read("puzzle_input.txt")
+sum = 0
+
+input.each_line do |line|
+  new_array = line.split("\t").map(&:to_i)
+  new_array.permutation(2) do |a, b|
+    if a % b == 0
+      value = a / b
+      puts value
+      sum += value
+    end
+  end
+end
+
+puts sum
