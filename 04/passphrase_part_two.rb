0aa15fbe410793a8b34aa051f2924d377bf388a9
Michael Ulrich <michaeldulrich@gmail.com>
2018-01-03 09:30:58 -0500

    Tests from andre

--- 04/passphrase_part_two.rb
+++ 04/passphrase_part_two.rb
@@ -1,7 +1,22 @@
-input = File.readlines('puzzle_input.txt')
-input_2d = input.each do |line|
-  line.split(' ')
-end
-valid_passphrases = 0
+# input = File.readlines('puzzle_input.txt')
+# input_2d = input.each do |line|
+#   line.split(' ')
+# end
+# valid_passphrases = 0
+#
+# puts input_2d[0][0]
 
-puts input_2d[0][0]
+class PassphraseChecker
+  def self.valid?(passphrase)
+    seen_words = {}
+
+    passphrase
+      .split(' ')
+      .each do |w|
+        return false if seen_words[w]
+        seen_words[w] = true
+      end
+
+    true
+  end
+end
--- 04/passphrase_test.rb
+++ 04/passphrase_test.rb
@@ -1,20 +1,25 @@
 require "minitest/autorun"
-# require_relative "passphrase_part_two.rb"
+require_relative "passphrase_part_two.rb"
 
 
 class TestPassphrase < Minitest::Test
-  def setup
-    @input = File.readlines('puzzle_input.txt').map(&:strip)
-    @input_2d = @input.each do |line|
-      line.split(" ")
-    end
+  # def setup
+  #   @input = File.readlines('puzzle_input.txt').map(&:strip)
+  # end
+  #
+  # def test_first_line
+  #   assert_equal @input[0], "vxjtwn vjnxtw sxibvv mmws wjvtxn icawnd rprh"
+  # end
+  def test_valid_passphrase
+    assert PassphraseChecker.valid?('aa bb cc dd ee')
   end
 
-  def test_first_line
-    assert_equal @input[0], "vxjtwn vjnxtw sxibvv mmws wjvtxn icawnd rprh"
+  def test_invalid_passphrase_with_repeated_word
+    refute PassphraseChecker.valid?('aa bb cc dd aa')
   end
 
-  def test_first_word
-    assert_equal @input_2d[0][0], "vxjtwn"
+  def test_valid_passphrase_with_similar_words
+    assert PassphraseChecker.valid?('aa bb cc dd aaa')
   end
+
 end
